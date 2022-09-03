a2ce8f538c2662dc21637f1dce61f76edd09b3a8
Niraj Gupta <nirajguptasde@gmail.com>
2022-09-03 17:27:30 +0530

    ex3 completed

---
+++ ex1.py
@@ -0,0 +1,10 @@
+print("Hello World!")
+print("Hello Again.")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+# Study Drills
+print("This is another line with double quotes.")
+print('This one is with "single" quotes.')
---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later
+# Anyting after # is ignored by the python
+
+print("I could have code like this") # and the comment after is ignored
+
+# you can also use a comment to disable a part of the program
+# print("this wont run")
+print("this will run")
+
